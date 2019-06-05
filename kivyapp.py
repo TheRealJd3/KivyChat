a899a568b991f002fcece0b3139b1e78c64f30a5
Jason <jasonshawndsouza@gmail.com>
2019-06-05 21:58:37 +0400

    Chat History Works

--- kivyapp.py
+++ kivyapp.py
@@ -21,7 +21,7 @@ class ScrollableLabel(ScrollView):
         self.layout= GridLayout(cols=1, size_hint_y=None)
         self.add_widget(self.layout)
 
-        self.chat_history = Label(size_hint_y=None, markup = True)
+        self.chat_history = Label(size_hint_y=None, markup=True)
         self.scroll_to_point=Label()
         self.layout.add_widget(self.chat_history)
         self.layout.add_widget(self.scroll_to_point)
@@ -31,7 +31,7 @@ class ScrollableLabel(ScrollView):
 
         self.layout.height = self.chat_history.texture_size[1] + 15
         self.chat_history.height = self.chat_history.texture_size[1]
-        self.chat_history.size = (self.chat_history.width*0.98,None)
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
         self.scroll_to(self.scroll_to_point)
 
 class ConnectPage(GridLayout):
@@ -41,7 +41,7 @@ class ConnectPage(GridLayout):
 
         if os.path.isfile("saved_details.txt"):
             with open("saved_details.txt", "r") as f:
-                s = f.read.split(",")
+                s = f.read().split(",")
                 
                 saved_ip = s[0]
                 saved_port = s[1]
@@ -114,7 +114,7 @@ class ChatPage(GridLayout):
         self.cols = 1
         self.rows = 2
         #Chat history bit
-        self.history = Label(height=Window.size[1]*0.9, size_hint_y= None)
+        self.history = ScrollableLabel(height=Window.size[1]*0.9, size_hint_y= None)
         self.add_widget(self.history)
         #Enter message
         self.new_message = TextInput(width=Window.size[0]*0.8, size_hint_x=None, multiline=False)
@@ -126,8 +126,29 @@ class ChatPage(GridLayout):
         message_area.add_widget(self.send)
         self.add_widget(message_area)
 
+        Window.bind(on_key_down=self.on_key_down)
+
+        Clock.schedule_once(self.focus_text_input,1)
+        socket_client.start_listening(self.incoming_message,show_error)
+
+    def on_key_down(self,instance,keyboard,keycode,text,modifiers):
+        if keycode == 40:
+            self.send_message(None)
+
     def send_message(self,_):
-        print("Send a message")
+        message = self.new_message.text
+        self.new_message.text = ""
+        if message:
+            self.history.update_chat_history(f"[color=dd2020]{chat_app.connect_page.username.text}[/color] > {message}")
+            socket_client.send(message)
+
+        Clock.schedule_once(self.focus_text_input, 0.1)
+
+    def focus_text_input(self,_):
+        self.new_message.focus= True
+
+    def incoming_message(self,username,message):
+        self.history.update_chat_history(f"[color=20dd20]{username}[/color] > {message}")
 
 
 
